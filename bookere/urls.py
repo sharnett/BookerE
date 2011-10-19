613709892516aac9974c71c4d1deed128c405743
Jack Reilly <jackdreilly@gmail.com>
2011-10-18 20:52:45 -0700

    wip

--- bookere/urls.py
+++ bookere/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from cloudmailin
+from cloudmailin.views import MailHandler
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
