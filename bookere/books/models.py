f424672a5ae310bea11caa0ba5e8086fb26bb8b5
Sean Harnett <srh2144@columbia.edu>
2011-11-09 14:32:41 -0500

    one last thing

--- bookere/books/models.py
+++ bookere/books/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 class Book(models.Model):
     friend_loan = models.CharField("Friend", max_length=50)
     date_loan = models.DateTimeField(auto_now_add=True,auto_now=True)
-    title = models.CharField("Title", max_length=200)
+    title = models.CharField("Book", max_length=200)
     user = models.ForeignKey(User,blank=False,null=False)
     active = models.BooleanField(default=True)
